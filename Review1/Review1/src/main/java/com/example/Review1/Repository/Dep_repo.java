d4fde2250f381d41a99670cd70d8866bc887ea6a
Suryank Chavan <158257044+suryank05@users.noreply.github.com>
2025-11-18 13:02:50 +0530

    Delete Review1/Review1 directory

--- Review1/Review1/src/main/java/com/example/Review1/Repository/Dep_repo.java
+++
@@ -1,9 +0,0 @@
-package com.example.Review1.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Review1.Entity.DepartmentEntity;
-
-public interface Dep_repo extends JpaRepository<DepartmentEntity, Long>{
-
-}
